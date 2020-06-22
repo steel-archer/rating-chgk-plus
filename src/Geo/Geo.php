b079a8988c81fd51ccab12f3ec9064d06f2ef8b7
Bohdan Sokulskyi <steelarcher@gmail.com>
2020-06-22 23:01:48 +0300

    Added Geo::getTowns method

--- src/Geo/Geo.php
+++ src/Geo/Geo.php
@@ -3,7 +3,6 @@
 namespace SteelArcher\RatingChgkPlus\Geo;
 
 use SteelArcher\RatingChgkPlus\Resource;
-use Symfony\Component\DomCrawler\Crawler;
 
 /**
  * Class Geo
@@ -16,8 +15,21 @@ class Geo extends Resource
      */
     public function getTowns(): array
     {
-        $crawler = new Crawler($this->get('geo.php?layout=town_list', []));
+        $result = [];
+        $rawCsv = trim($this->get('geo.php?layout=town_list&download_data=export_towns', []));
+        $csvArr = explode("\n", $rawCsv);
+        unset($csvArr[0]);
+        foreach ($csvArr as $csvLine) {
+            $data = str_getcsv($csvLine, ',', '"');
+            $result[$data[1]] = [
+                'id'       => $data[0],
+                'region'   => $data[2],
+                'country'  => $data[3],
+                'timezone' => $data[4],
+            ];
+        }
+        ksort($result);
 
-        return [];
+        return $result;
     }
 }
